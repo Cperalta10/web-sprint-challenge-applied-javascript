af34815fe340761de22addaabd91032c51842dc2
Christopher Peralta <94395580+Cperalta10@users.noreply.github.com>
2022-12-01 13:00:20 -0800

    finished tabs component

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,7 +15,18 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
+  const divTopics = document.createElement("div");
+  divTopics.classList.add("topics");
+
+  for (let i = 0; i < topics.length; i++) {
+    const topicsMade = document.createElement("div");
+    topicsMade.classList.add("tab");
+    topicsMade.textContent = topics[i];
+    divTopics.appendChild(topicsMade);
+  }
+
+  return divTopics;
+};
 
 const tabsAppender = (selector) => {
   // TASK 4
@@ -23,6 +36,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
+  axios
+    .get("http://localhost:5001/api/topics")
+    .then((res) => {
+      document.querySelector(selector).appendChild(Tabs(res.data.topics));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-export { Tabs, tabsAppender }
+export { Tabs, tabsAppender };
